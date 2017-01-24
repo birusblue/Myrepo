af290a2ffb8a59bbb1cf055dad36925cf789a11d
Nuggets <Nuggets@DE>
2017-01-24 12:41:22 +0530

    comment

--- recoganisation/src/main/java/pageObjects/Login.java
+++ recoganisation/src/main/java/pageObjects/Login.java
@@ -1,9 +1,14 @@
 package pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Login {
 	public static void main(String[] args)
 	{
-		
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://www.gmail.com");
+		//comment	
 	}
 
 }
